95a67e7a65f55f3e1d1f4996f8d12da3cc95559a
Joe Lag <71555652+hitsfm@users.noreply.github.com>
2023-03-13 20:23:41 -0700

    Add files via upload

---
+++ framed.php
@@ -0,0 +1,90 @@
+<?php
+
+$search_query = 'old radios';
+$url = 'https://www.google.com/search?q=' . urlencode($search_query) . '&tbm=isch';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$html = curl_exec($ch);
+if ($html === false) {
+    echo 'Error retrieving data from Google Images';
+    exit();
+}
+
+curl_close($ch);
+
+preg_match_all('/<img[^>]+>/i', $html, $matches);
+$image_tags = $matches[0];
+
+if (count($image_tags) == 0) {
+    echo 'No image results found';
+    exit();
+}
+
+$filtered_images = array();
+foreach ($image_tags as $tag) {
+    preg_match('/src="([^"]+)/i', $tag, $image);
+    preg_match('/alt="([^"]+)/i', $tag, $alt);
+    preg_match('/class="([^"]+)/i', $tag, $class);
+    preg_match('/width="([^"]+)/i', $tag, $width);
+    preg_match('/height="([^"]+)/i', $tag, $height);
+    $size = (int) $width[1] * (int) $height[1];
+    if (strpos($class[1], 'rg_i rg_sz') === false && $size >= 50000) {
+        $filtered_images[] = array('url' => $image[1], 'alt' => $alt[1]);
+    }
+}
+
+if (count($filtered_images) == 0) {
+    echo 'No image results found';
+    exit();
+}
+
+$random_image = $filtered_images[array_rand($filtered_images)];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Random Image</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+        }
+        #frame {
+            position: relative;
+            width: 30%;
+            max-width: 800px;
+            height: 50%;
+            max-height: 800px;
+            margin: auto;
+            overflow: hidden;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+        }
+        img {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div id="frame">
+        <a href="<?php echo $random_image['url']; ?>" target="_blank">
+            <img src="<?php echo $random_image['url']; ?>" alt="<?php echo $random_image['alt']; ?>">
+        </a>
+    </div>
+</body>
+</html>
